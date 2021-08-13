4551a961c697c0fac32b46906ae7a797d3b2386e
Kritebh <kritebhlaganbibhakar@gmail.com>
2021-08-13 14:45:21 +0530

    axios done

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -41,9 +41,13 @@ INSTALLED_APPS = [
     'base.apps.BaseConfig',
     #3rd party apps 
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -122,6 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
