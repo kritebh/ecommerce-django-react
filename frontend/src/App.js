3a413bd5a7148eeb235278bc0ecb26d4b0177726
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2021-08-16 10:57:10 +0530

    25. Qty Select & Add To Cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
 
 /* REACT ROUTER */
 import { BrowserRouter as Router, Route } from "react-router-dom";
@@ -18,6 +19,7 @@ function App() {
         <main className="py-3">
           <Route exact path="/" component={HomeScreen} />
           <Route path="/product/:id" component={ProductScreen} />
+          <Route path="/product/:id?" component={CartScreen} />
         </main>
       </Container>
       <Footer />
---
+++ frontend/src/screens/CartScreen.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CartScreen() {
+  return <div>Cart</div>;
+}
+
+export default CartScreen;
