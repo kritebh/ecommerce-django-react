e29b98c5f9e60a082bcd2f52b0e0fa10043b60b5
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2021-08-16 11:52:29 +0530

    27. Add To Cart Functionality

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,6 +1,49 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-function CartScreen() {
+/* REACT ROUTER */
+import { Link } from "react-router-dom";
+
+/* REACT BOOTSTRAP */
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Button,
+  Card,
+  Form,
+} from "react-bootstrap";
+
+/* COMPONENTS */
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+
+/* REACT - REDUX */
+import { useDispatch, useSelector } from "react-redux";
+
+/* ACTION CREATORS */
+import { addToCart } from "../actions/productActions";
+
+function CartScreen({ match, location, history }) {
+  /* GETTING DATA FROM URL IF PRESENT */
+  const productId = match.params.id;
+
+  const qty = location.search ? Number(location.search.split("=")[1]) : 1;
+  //qty: '?qty=3' -> ['?qty',3] -> 3
+
+  /* FIRING OFF DISPATCH, BUT ONLY IF WE HAVE A PRODUCT ID & QUANTITY */
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty]);
+
+  /* PULLING A PART OF STATE FROM THE ACTUAL STATE IN THE REDUX STORE */
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
   return <div>Cart</div>;
 }
 
