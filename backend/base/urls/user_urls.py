33ee14447a91ce73826be2cc95228043c2bd69df
Kritebh <kritebhlaganbibhakar@gmail.com>
2021-08-17 12:39:19 +0530

    user profile update section 7 #43

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('register/',views.registerUser,name='register'),
     path('',views.getUsers,name="users"),
     path('profile/',views.getUserProfile,name="user_profile"),
+    path('profile/update/',views.updateUserProfile,name="user_profile_update"),
     path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -91,6 +91,21 @@ def getUserProfile(request):
     return Response(serializer.data)
 
 
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def updateUserProfile(request):
+    user =request.user 
+    serializer = UserSerializerWithToken(user,many = False)
+    data = request.data
+    user.first_name = data['name']
+    user.username = data['email']
+    user.email = data['email']
+    if data['password'] !="":
+        user.password= make_password(data['password'])
+    user.save()
+    return Response(serializer.data)
+
+
 @api_view(['GET'])
 @permission_classes([IsAdminUser])
 def getUsers(request):
