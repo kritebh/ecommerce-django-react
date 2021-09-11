e9aa60ddc0a4aebc48d935f8222d081daae37ce0
Kritebh <kritebhlaganbibhakar@gmail.com>
2021-09-11 12:05:29 +0530

    right alignment fixed

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -33,7 +33,7 @@ function Header() {
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
           <LinkContainer to="/">
-            <Navbar.Brand>Dukkan</Navbar.Brand>
+            <Navbar.Brand>ProShop</Navbar.Brand>
           </LinkContainer>
 
           <Navbar.Toggle aria-controls="navbarScroll" />
